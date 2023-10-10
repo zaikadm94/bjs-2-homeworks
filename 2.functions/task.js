0ab7ba655f1b823033724ed2d6c9a08f54a0becf
zaikadm94 <zaikadm94@mail.ru>
2023-10-10 14:52:30 +0300

    решение задания

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,15 @@
 function getArrayParams(...arr) {
+  if(!arr || !arr.length) return null;
+  const max =  Math.max(arr);
+  const min = Math.min(arr);
+  const sum = arr?.reduce((acc, curr) => acc + curr, 0);
+  const avg = sum / arr.length;
+
+
+
+
+
+
 
   return { min: min, max: max, avg: avg };
 }
