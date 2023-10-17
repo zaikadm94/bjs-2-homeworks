6979f2f2e2862d3538b3063b9f8e25a70c628216
zaikadm94 <zaikadm94@mail.ru>
2023-10-18 00:34:15 +0300

    решение задач

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -5,35 +5,35 @@ function Student(name, gender, age) {
     this.marks = [];
 }
 
-    let student1 = new Student("Ivan", "male", 15);
-    let student2 = new Student("Anna", "female", 18);
+let student1 = new Student("Ivan", "male", 15);
+let student2 = new Student("Anna", "female", 18);
 
-    Student.prototype.setSubject = function (subjectName) {
-        this.subject = subjectName;
+Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+}
+
+
+Student.prototype.addMarks = function (...marksToAdd) {
+    if (!this.marks) {
+        console.log(`${this.name} отчислен.`);
+    } else {
+        this.marks.push(...marksToAdd);
     }
 
+}
 
-    Student.prototype.addMarks = function (...marksToAdd) {
-        if(!this.marks) {
-            console.log(`${this.name} отчислен.`);
-        } else {
-            this.marks.push(...marksToAdd);
-        }
-
+Student.prototype.getAverage = function () {
+    if (!this.marks || this.marks.length === 0) {
+        return 0;
+    } else {
+        return this.marks.reduce((acc, mark) => acc + mark, 0) / this.marks.length;
     }
 
-    Student.prototype.getAverage = function () {
-        if(!this.marks || this.marks.length === 0){
-            return 0;
-        } else {
-            return this.marks.reduce((acc, mark) => acc + mark, 0) / this.marks.length;
-        }
+}
 
-    }
+Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 
-    Student.prototype.exclude = function (reason) {
-        delete this.subject;
-        delete  this.marks;
-        this.excluded = reason;
-
-    }
+}
