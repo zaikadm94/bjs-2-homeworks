45aa283c0537bbe68d0e1b640584eb8258c39c47
zaikadm94 <zaikadm94@mail.ru>
2023-10-17 22:07:51 +0300

    решение задания 1 и 2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false
+    }
+    return arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const userFilter = users.filter(user => user.gender === gender);
+    if (userFilter.length === 0) {
+        return 0;
+    }
+    const ages = userFilter.map(users => users.age);
+    const totalAges = ages.reduce((sum, age) => sum + age, 0);
+    const averageAge = totalAges / userFilter.length;
+    return averageAge;
+
+
 }
